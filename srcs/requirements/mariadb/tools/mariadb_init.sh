237fe247641770ad5569d7ad94733a33ea9e5c98
Chloekkk <skdusdl8804@gmail.com>
2023-01-08 22:17:25 +0900

    ✨ feat: Update mariadb Dockerfile execution
    Update mariadb Dockerfile execution

--- srcs/requirements/mariadb/tools/mariadb_init.sh
+++ srcs/requirements/mariadb/tools/mariadb_init.sh
@@ -3,15 +3,13 @@
 # It is called by the mariadb container when it starts
 
 # Exit on error
-set -ex
+set -e
 
 # TODO: if 조건문 좋은 방법 고민해보기
 # Create the database if it does not exist
-if [! -d /var/lib/mysql/${WORDPRESS_DB_NAME}]; then
+if [ ! -d /var/lib/mysql/${WORDPRESS_DB_NAME} ]; then
 	# install initial database
 	mysql_install_db --user=mysql --datadir=/var/lib/mysql
-	# set initial root password
-	mysqladmin -u ${MARIADB_ROOT_NAME} password ${MARIADB_ROOT_PASSWORD}
 	# start mariadb daemon in safe mode
 	/usr/bin/mysqld_safe --datadir='/var/lib/mysql' &
 	# wait for mariadb to start
@@ -19,8 +17,16 @@ if [! -d /var/lib/mysql/${WORDPRESS_DB_NAME}]; then
 		echo "Could not connect to mariadb"
 		exit 1
 	fi
+	# set initial root password
+	mysqladmin -u ${MARIADB_ROOT_NAME} password ${MARIADB_ROOT_PASSWORD}
 	# create user and wordpress database
 	eval "echo \"$(cat /tmp/wordpress_init.sql)\"" | mariadb
 	# stop mariadb daemon
 	mysqladmin -u${MARIADB_ROOT_NAME} -p${MARIADB_ROOT_PASSWORD} shutdown
+
+	echo "Mariadb initialization complete"
 fi
+
+echo "Starting mariadb daemon"
+# start mariadb daemon
+exec /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
