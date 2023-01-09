63c313f4eba8a6a158fe44d310045800ffcef503
Chloekkk <skdusdl8804@gmail.com>
2023-01-09 14:12:47 +0900

    ✨ feat: Update create var/log/mysql/error.log command in entrypoint script
    Update create var/log/mysql/error.log command in entrypoint script and update mariadb client user and password

--- srcs/requirements/mariadb/tools/mariadb_init.sh
+++ srcs/requirements/mariadb/tools/mariadb_init.sh
@@ -1,14 +1,20 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 # This script is used to initialize the mariadb database
 # It is called by the mariadb container when it starts
 
 # Exit on error
-set -e
+set -ex
 
 # Create the database if it does not exist
 if [ ! -d /var/lib/mysql/${WORDPRESS_DB_NAME} ]; then
+	# create /var/log/mysql/error.log if it does not exist
+	mkdir -p /var/log/mysql
+	touch /var/log/mysql/error.log
+	# change ownership of /var/lib/mysql to mysql
+	chown -R mysql:mysql /var/log/mysql
+	chown -R mysql:mysql /var/lib/mysql
 	# install initial database
-	mysql_install_db --user=mysql --datadir=/var/lib/mysql
+	mysql_install_db --user=mysql --auth-root-authentication-method=normal
 	# start mariadb daemon in safe mode
 	/usr/bin/mysqld_safe --datadir='/var/lib/mysql' &
 	# wait for mariadb to start
@@ -18,8 +24,8 @@ if [ ! -d /var/lib/mysql/${WORDPRESS_DB_NAME} ]; then
 	fi
 	# set initial root password
 	mysqladmin -u ${MARIADB_ROOT_NAME} password ${MARIADB_ROOT_PASSWORD}
-	# create user and wordpress database
-	eval "echo \"$(cat /tmp/wordpress_init.sql)\"" | mariadb
+	# create user and wordpress database & secure execution sql file
+	eval "echo \"$(cat /tmp/wordpress_init.sql)\"" | mariadb -u${MARIADB_ROOT_NAME} -p${MARIADB_ROOT_PASSWORD}
 	# stop mariadb daemon
 	mysqladmin -u${MARIADB_ROOT_NAME} -p${MARIADB_ROOT_PASSWORD} shutdown
 
