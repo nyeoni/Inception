cad780e7d48f697ec0f071f5f9441c8581521661
Chloekkk <skdusdl8804@gmail.com>
2023-01-09 14:04:57 +0900

    ✨ feat: Update Delete test database query

--- srcs/requirements/mariadb/tools/wordpress_init.sql
+++ srcs/requirements/mariadb/tools/wordpress_init.sql
@@ -4,5 +4,8 @@ CREATE DATABASE IF NOT EXISTS $WORDPRESS_DB_NAME;
 CREATE USER '${WORDPRESS_DB_USER}'@'%' IDENTIFIED BY '${WORDPRESS_DB_PASSWORD}';
 -- Grant privileges to Wordpress User
 GRANT ALL PRIVILEGES ON ${WORDPRESS_DB_NAME}.* TO '${WORDPRESS_DB_USER}'@'%';
--- FLUSH PRIVILEGES;
+-- Delete Test Database
+DROP DATABASE IF EXISTS test;
+-- Apply changes
+FLUSH PRIVILEGES;
 -- Privileges assigned through GRANT option do not need FLUSH PRIVILEGES to take effect - MySQL server will notice these changes and reload the grant tables immediately.
