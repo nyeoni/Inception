39ddc86b85dc451cf9fd340f61af7deb36dc2742
Chloekkk <skdusdl8804@gmail.com>
2023-01-10 11:41:29 +0900

    ✨ feat: Update wordpress core install and create user scripts

--- srcs/requirements/wordpress/tools/wordpress_init.sh
+++ srcs/requirements/wordpress/tools/wordpress_init.sh
@@ -20,7 +20,23 @@ fi
 # Create wordpress config file
 if [ ! -f /var/www/wordpress/wp-config.php ]; then
 	# create wordpress config file
-	wp core config --path=/var/www/wordpress --dbname=${WORDPRESS_DB_NAME} --dbuser=${WORDPRESS_DB_USER} --dbpass=${WORDPRESS_DB_PASSWORD} --dbhost=${MARIADB_HOST} --extra-php < /tmp/wp-config
+	wp core config --user=www-data --path=/var/www/wordpress --dbname=${WORDPRESS_DB_NAME} --dbuser=${WORDPRESS_DB_USER} --dbpass=${WORDPRESS_DB_PASSWORD} --dbhost=${MARIADB_HOST} --extra-php < /tmp/wp-config
+	echo 'Wordpress Config file created'
+else
+	echo 'Wordpress Config file already exists'
+fi
+
+# Install wordpress
+if ! wp core is-installed --user=www-data; then
+	# create wordpress tables in the database and create admin user
+	wp core install --user=www-data \
+	--url=${DOMAIN_NAME} \
+	--title=${WORDPRESS_TITLE} \
+	--admin_user=${WORDPRESS_ADMIN_USER} \
+	--admin_password=${WORDPRESS_ADMIN_PASSWORD} \
+	--admin_email=${WORDPRESS_ADMIN_EMAIL}
+	# create user
+	wp user create --user=www-data ${WORDPRESS_USER} ${WORDPRESS_USER_EMAIL} --role=author --user_pass=${WORDPRESS_USER_PASSWORD}
 fi
 
 # Excute arg
