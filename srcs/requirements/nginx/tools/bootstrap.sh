0cea261683d4bef8007a7e47f7a60f4ba5d49223
Chloekkk <skdusdl8804@gmail.com>
2023-01-12 09:09:05 +0900

    ✨ feat: Add default starting script in nginx bootstrap.sh

--- srcs/requirements/nginx/tools/bootstrap.sh
+++ srcs/requirements/nginx/tools/bootstrap.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env sh
 
+# Check arg
+if [ "$#" -eq 0 ]; then
+	echo "Nginx is starting..."
+	nginx -g 'daemon off;'
+	exit 0
+fi
+
 # Bootstrap nginx
-if [ $@ -eq 'log' ]; then
+if [ "$@" = "log" ]; then
 	# run nginx as service
+	echo "Nginx is starting..."
 	nginx
 	# print nginx log
 	tail -f /var/log/nginx/access.log /var/log/nginx/error.log
