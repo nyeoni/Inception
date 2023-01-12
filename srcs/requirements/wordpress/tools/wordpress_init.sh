d161a4741d961b872d58485010a582b6ab5cd427
Chloekkk <skdusdl8804@gmail.com>
2023-01-12 09:10:36 +0900

    ✨ feat: Update worpress_init.sh waiting mariadb logic
    #

--- srcs/requirements/wordpress/tools/wordpress_init.sh
+++ srcs/requirements/wordpress/tools/wordpress_init.sh
@@ -3,31 +3,35 @@
 # It is called by the wordpress container when it starts
 
 # Exit on error
-set -ex
+set -x
 
 # Create wordpress directory
 if [ ! -d /var/www/wordpress ]; then
 	# create wordpress directory if not exists
 	mkdir -p /var/www/wordpress
+fi
+
+# Check mariadb server
+mariadb-admin ping  -u${MARIADB_ROOT_NAME} -p${MARIADB_ROOT_PASSWORD} --host=${MARIADB_HOST} --wait=1 --connect-timeout=30
+
+# Install wordpress
+if ! wp core is-installed --allow-root --path=/var/www/wordpress; then
 	# download latest wordpress to wordpress path
 	wp core download --path=/var/www/wordpress
 	# create www-data system user if not exists
 	adduser -D -S -H -G www-data www-data
 	# change ownership of wordpress directory to www-data
 	chown -R www-data:www-data /var/www/wordpress
-fi
 
-# Create wordpress config file
-if [ ! -f /var/www/wordpress/wp-config.php ]; then
 	# create wordpress config file
-	wp core config --allow-root --path=/var/www/wordpress --dbname=${WORDPRESS_DB_NAME} --dbuser=${WORDPRESS_DB_USER} --dbpass=${WORDPRESS_DB_PASSWORD} --dbhost=${MARIADB_HOST} --extra-php < /tmp/wp-config
-	echo 'Wordpress Config file created'
-else
-	echo 'Wordpress Config file already exists'
-fi
+	if [ ! -f /var/www/wordpress/wp-config.php ]; then
+		# create wordpress config file
+		wp core config --allow-root --path=/var/www/wordpress --dbname=${WORDPRESS_DB_NAME} --dbuser=${WORDPRESS_DB_USER} --dbpass=${WORDPRESS_DB_PASSWORD} --dbhost=${MARIADB_HOST} --extra-php < /tmp/wp-config
+		echo 'Wordpress Config file created'
+	else
+		echo 'Wordpress Config file already exists'
+	fi
 
-# Install wordpress
-if ! wp core is-installed --allow-root --path=/var/www/wordpress; then
 	# create wordpress tables in the database and create admin user
 	wp core install --allow-root --path=/var/www/wordpress \
 	--url=${DOMAIN_NAME} \
@@ -40,7 +44,7 @@ if ! wp core is-installed --allow-root --path=/var/www/wordpress; then
 fi
 
 # Excute arg
-if [ $@ -eq 'log' ]; then
+if [ "$@" -eq 'log' ]; then
 	# Start php-fpm
 	php-fpm8
 	# Print nginx logs
